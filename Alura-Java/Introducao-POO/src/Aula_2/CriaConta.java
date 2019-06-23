d3c29cb6737fc410f0334b38c53e4e024a1dff42
mauriciocoruja <mbjr@outlook.com.br>
2019-06-23 14:29:43 -0300

    Aula 2 - Criando uma segunda instancia

--- Alura-Java/Introducao-POO/src/Aula_2/CriaConta.java
+++ Alura-Java/Introducao-POO/src/Aula_2/CriaConta.java
@@ -6,5 +6,16 @@ public class CriaConta {
 
         primeirConta.saldo = 200;
         System.out.println(primeirConta.saldo);
+
+        primeirConta.saldo += 100;
+        System.out.println(primeirConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+
+        System.out.println("Na segunda conta tem R$"+segundaConta.saldo);
+        System.out.println("Na primeira conta tem R$"+primeirConta.saldo);
+
     }
 }
