00f1bd78684bca45d895a9fcc3e898bc136ace01
mauriciocoruja <mbjr@outlook.com.br>
2019-06-24 21:13:57 -0300

    Aula 3 - Métodos com referência e mais retorno

--- Alura-Java/Introducao-POO/src/Aula_2/Conta.java
+++ Alura-Java/Introducao-POO/src/Aula_2/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
     public double saldo;
     public int agencia;
     public int numero;
-    private String titular;
+    public String titular;
 
     public void deposita(double valor){
         this.saldo += valor;
@@ -17,5 +17,15 @@ public class Conta {
             System.out.println("Você não tem saldo o suficiente");
             return false;
         }
+    }
+
+    public double transfere(double valor, Conta destino){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+        }else {
+            System.out.println("Você não tem saldo");
+        }
+        return valor;
     }
 }
--- Alura-Java/Introducao-POO/src/Aula_2/TestaMetodo.java
+++ Alura-Java/Introducao-POO/src/Aula_2/TestaMetodo.java
@@ -3,6 +3,7 @@ package Aula_2;
 public class TestaMetodo {
     public static void main(String[] args) {
         Conta contaDoCoruja = new Conta();
+        contaDoCoruja.titular = "Coruja";
 
         contaDoCoruja.saldo = 100;
         contaDoCoruja.deposita(50);
@@ -11,5 +12,15 @@ public class TestaMetodo {
 
         contaDoCoruja.saca(100);
         System.out.println(contaDoCoruja.saldo);
+
+        Conta contaDoMauricio = new Conta();
+        contaDoMauricio.titular  = "Mauricio";
+        contaDoMauricio.deposita(1000);
+
+        double valorTransferencia = contaDoMauricio.transfere(500, contaDoCoruja);
+        System.out.println(contaDoCoruja.titular+" recebeu R$"+valorTransferencia+" de "+contaDoMauricio.titular);
+
+        System.out.println(contaDoCoruja.titular + " | saldo de R$" + contaDoCoruja.saldo);
+        System.out.println(contaDoMauricio.titular + " | saldo R$" + contaDoMauricio.saldo);
     }
 }
