d6c6ffbbbb8fcf5880b7fe8b672e2224400128f9
mauriciocoruja <mbjr@outlook.com.br>
2019-06-29 19:06:29 -0300

    Projeto criado. Incluindo dependencias Spring Boot 2, Lombok, JPA e MySQL

---
+++ Extra/sucos/src/main/java/br/com/sucobao/sucos/SucosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sucobao.sucos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucosApplication.class, args);
+	}
+
+}
---
+++ Extra/sucos/src/test/java/br/com/sucobao/sucos/SucosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.sucobao.sucos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SucosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
