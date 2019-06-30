d3ab40d5cdb8d554ada47f961985e21392e6d38d
mauriciocoruja <mbjr@outlook.com.br>
2019-06-29 21:49:38 -0300

    Criada a classe ClienteRepository, e extendida a classe JpaRepository, que fornecerá os métodos para realização das operações do CRUD na tabela

--- Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/ClienteRepository.java
+++ Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/ClienteRepository.java
@@ -1,6 +1,9 @@
 package br.com.sucobao.sucos.tbCliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
+
 }
