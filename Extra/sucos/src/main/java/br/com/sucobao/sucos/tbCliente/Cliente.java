b5e62175e6c1fae1465e088b63e1d09128791890
mauriciocoruja <mbjr@outlook.com.br>
2019-06-30 01:05:46 -0300

    Ajustado os nomes dos atributos para ficarem  adequados com a tabela usada; a anotação @Temporal(Temporal.DATE) usada para especificar o formato da data e se adequar ao que está no banco

--- Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/Cliente.java
+++ Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/Cliente.java
@@ -3,21 +3,17 @@ package br.com.sucobao.sucos.tbCliente;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.security.Identity;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tbCliente")
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String cpf;
 
     @NotNull
@@ -30,17 +26,10 @@ public class Cliente {
     private String cep;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "DATA_NASCIMENTO")
-    //TODO Verificar se a Classe dada abaixo não vai dar ruim, caso ocorra, tentar mudar para a import java.sql.Date;
-    private Date dataNascimento;
+    private Date datanascimento;
     private short idade;
-    @Column (name = "LIMITE_CREDITO")
-    private float limiteCredito;
-
-    @Column (name = "LIMITE_COMPRA")
-    private float limiteCompra;
-
-    @Column (name = "PRIMEIRA_COMPRA")
-    private byte primeiraCompra;
+    private float limitecredito;
+    private float volumecompra;
+    private byte primeiracompra;
 
 }
