47d4ac703221dcc1865c4ea38ff6a239a9a75056
mauriciocoruja <mbjr@outlook.com.br>
2019-06-30 01:14:44 -0300

    Criado os métodos de criar Cliente e Salvar no banco de dados. Usada a anotação @RequestBody, que vai recolher o conteudo da requisição e transformá-lo em um objeto da classe Cliente, possibilitando o registro adequado na tabela

--- Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/ClienteController.java
+++ Extra/sucos/src/main/java/br/com/sucobao/sucos/tbCliente/ClienteController.java
@@ -1,6 +1,34 @@
 package br.com.sucobao.sucos.tbCliente;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/clientes"})
 public class ClienteController {
+
+    private ClienteRepository clienteRepository;
+
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    // C do CRUD
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    // R do CRUD
+    @GetMapping
+    public List findAll(){
+       return clienteRepository.findAll();
+    }
+
+    //
+
 
 
 }
