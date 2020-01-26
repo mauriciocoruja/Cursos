6e1289c69c5dd3ce8e09fb23c4e6291cef6c2448
mauriciocoruja <mbjr@outlook.com.br>
2020-01-26 16:59:53 -0300

    Revisando conceitos de condicionais

--- Alura-Java/Introducao-Java/src/TestaCondicional2.java
+++ Alura-Java/Introducao-Java/src/TestaCondicional2.java
@@ -2,11 +2,22 @@ public class TestaCondicional2 {
     public static void main(String[] args) {
         System.out.println("Testando condicionais");
 
-        int idade = 10;
-        int quantidadedePessoas = 1;
+        int idade = 17;
+        int quantidadedePessoas = 2;
 
+        boolean acompanhado = quantidadedePessoas >= 2;
 
-        if (idade >= 18 && quantidadedePessoas >= 2) {
+        String resposta;
+
+        if (acompanhado){
+            resposta = "Sim";
+        } else {
+            resposta = "Não";
+        }
+
+        System.out.println("Tem acompanhante: " + resposta);
+
+        if (idade >= 18 || acompanhado) {
             System.out.println("Seja bem vindo");
         } else {
             System.out.println("Infelizmente você não é bem vindo");
