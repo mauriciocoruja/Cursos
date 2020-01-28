0dad55ad78c9cab6fe2a00c8d0873e9df51c49d7
mauriciocoruja <mbjr@outlook.com.br>
2020-01-28 20:48:09 -0300

    Revisando conceitos básicos: laços com condicionais. Finalizando desafio para imprimir multiplos de 3.

---
+++ Alura-Java/Introducao-Java/src/Desafio1.java
@@ -0,0 +1,10 @@
+public class Desafio1 {
+    public static void main(String[] args) {
+        int numero;
+        for (numero = 1; numero <= 100; numero++) {
+            if (numero % 10 == 0) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
